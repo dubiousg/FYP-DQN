9d5725fcc03c48720f03ea3ef5e1744f3375f79f
dubiousg <37251424+dubiousg@users.noreply.github.com>
2020-03-30 15:42:15 +0100

    completing model

--- temp.py
+++ temp.py
@@ -1,3 +1,13 @@
 import datetime
-print(datetime.datetime.now().strftime("%Y%m%d-%H%M%S"))
+#print(datetime.datetime.now().strftime("%Y%m%d-%H%M%S"))
+stock_data = {}
 
+li = []
+for i in range(10):
+    li.append(i)
+    stock_data[str(i)] = li
+
+print(stock_data)
+total_days = len(max(stock_data.values(), key = lambda x: len(x)))
+
+print(total_days)
