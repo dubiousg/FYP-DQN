0132b93bbb22036aa20082ff68dbf204613c3c5e
dubiousg <37251424+dubiousg@users.noreply.github.com>
2020-03-30 23:20:27 +0100

    Add files via upload

--- model/market.py
+++ model/market.py
@@ -9,16 +9,25 @@ import sys
 day_global = 0 #day should correspond to the index of the current day
 stock_data = {} #contains stock data of the market
 
+
 class Portfolio:
-    stocks = {} #stock name : #stocks
-    cash = 1000000
-    total_value = cash
     
-    def __init__(self):
+    def __init__(self, stock_names):
         print("initialising portfolio")
+        self.stocks = {} #stock name : #stocks
+        self.cash = 1000000
+        self.total_value = self.cash
+
+        for stock in stock_names:
+            self.stocks[stock] = 0
+
+        print(self.stocks)
         #todo
     
     def update_stock(self, stock, volume, cash_change):
+        #print("current stock:" + stock)
+        #print(self.stocks)
+        #print(stock in self.stocks.keys())
         self.stocks[stock] += volume
         self.cash += cash_change
 
@@ -26,7 +35,7 @@ class Portfolio:
         global day_global
         temp_total = self.cash
         for stock in self.stocks:
-            temp_total += (self.stocks[stock] * stock_data[stock][day_global]['close'])
+            temp_total += (self.stocks[stock] * stock_data[stock].iloc[day_global]['close'])
 
         self.total_value = temp_total
 
@@ -34,14 +43,16 @@ class Portfolio:
         return self.total_value
 
     def reset(self):
-        self.stocks = {} #stock name : #stocks
+        for stock in self.stocks.keys():
+            self.stocks[stock] = 0
+        #self.stocks = {} #stock name : #stocks
         self.cash = 1000000
         self.total_value = self.cash
 
-portfolio = Portfolio()#contains the trader's portfolio info: stocks, cash, total value,  
+#contains the trader's portfolio info: stocks, cash, total value,  
 
 class Market_Environment:
-
+    portfolio = None
     #read in the clean stock and store them
     def __init__(self, stocks_folder):
         print("initialising market")
@@ -62,24 +73,27 @@ class Market_Environment:
             self.stock_names.append(key)
 
         self.total_days = len(max(stock_data.values(), key = lambda x: len(x)))
+        self.portfolio = Portfolio(self.stock_names)
     #provide portfolio data, (from this possible actions can be computed)
     
-    def compute_rewards(self, portfolio):
-        prev_value = portfolio.get_total_value()
-        portfolio.update_value()
-        new_value = portfolio.get_total_value()
+    def compute_rewards(self):
+        prev_value = self.portfolio.get_total_value()
+        self.portfolio.update_value()
+        new_value = self.portfolio.get_total_value()
         reward = new_value - prev_value
         return reward
 
     def get_observations(self):
-        #think about returning a dataframe
+        #returns a list of dataframes
         global day_global
-        next_state = {}
+        next_state = []
         #should return largest length of all stocks
         if day_global + 1 < self.total_days:
-            for stock in stock_data:
-                next_state[stock] = stock_data[stock].iloc[day_global + 1]
-                next_state[stock].drop(columns=['Name', 'date'], axis=1)
+            i = 0
+            for data in stock_data.values():
+                next_state.append(data.iloc[day_global + 1])
+                next_state[i].drop(columns=['Name', 'date'], axis=1)
+                i += 1
         return next_state
 
     #represents a day of trading: 
@@ -101,17 +115,17 @@ class Market_Environment:
         for stock, action in actions_dict.items():            
             actions_dict[stock] =  round(action * float_int_scaler) #round to make sure it is an integer
 
-        for stock in actions_dict:           
-            volume = actions_dict[stock] #volume bought or sold: + for bought - for sold
+        for stock, action in actions_dict.items():           
+            volume = action #volume bought or sold: + for bought - for sold
             cash_change = stock_data[stock].iloc[day_global]['close'] #the cash recieved or taken from buying or selling a stock 
             cash_change = cash_change * (- volume)
-            portfolio.update_stock(stock, volume, cash_change)    
-        
+            self.portfolio.update_stock(stock, volume, cash_change)    
+
         day_global += 1
         
         observations = self.get_observations()
         
-        reward = self.compute_rewards(portfolio)
+        reward = self.compute_rewards()
 
         done = (day_global + 1 == self.total_days)
 
@@ -120,7 +134,7 @@ class Market_Environment:
     def reset(self):
         global day_global
         day_global = 0
-        portfolio.reset()
+        self.portfolio.reset()
 
         return self.get_observations()
 
