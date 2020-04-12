488213817abdf8516a3230f8d51da3eb8f4863c7
dubiousg <37251424+dubiousg@users.noreply.github.com>
2020-04-12 17:17:47 +0100

    minor changes

--- temp.py
+++ temp.py
@@ -1,9 +1,18 @@
 import datetime
 import numpy as np
 import tensorflow as tf
+import math
 from benchmarking.timer import Timer
-arr = np.atleast_2d(np.array([float('nan'), 123, 12.3], dtype='float32'))
-a = tf.convert_to_tensor(arr)
-print(a[0])
+
+arr = np.array([1, 2, 3, 0, -1, float("nan")])
+total = 0
+for i in range(arr.size):
+    if math.isnan(arr[i]) or arr[i] < 0:
+        arr[i] = 0
+
+    total += arr[i]
+
+
+abc = 0
 #print(datetime.datetime.now().strftime("%Y%m%d-%H%M%S"))
 
