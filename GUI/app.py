a35c8a3122b14554c35a0b87d5ab721e3939e0e8
dubiousg <37251424+dubiousg@users.noreply.github.com>
2020-04-18 00:41:00 +0100

    matplotlib basics

--- GUI/app.py
+++ GUI/app.py
@@ -1,33 +1,83 @@
-import tkinter as tk
-from tkinter import *
 import os
+import matplotlib.pyplot as plt
+import tkinter as tk
+from model.market import Market_Environment
+import model.trader as trader
+from tkinter import *
 
-root = tk.Tk()
+class GUI:
 
-height = 800
-width = 1200
-canvas = tk.Canvas(root, height=height, width=width, bg="#263d42")
-canvas.pack()
+    def __init__(self):
+        self.states = { "testing": False, "tested": False } #state of the gui
+        self.root = tk.Tk()
+        self.height = 800
+        self.width = 1200
 
-frame = tk.Frame(root, bg="#3e646c")
+        self.canvas = tk.Canvas(self.root, height=self.height, width=self.width, bg="#263d42")
+        self.canvas.pack()
 
-frame.place(relwidth=0.85, relheight=0.85, rely=0.1, relx=0.1)
+        self.frame = tk.Frame(self.root, bg="#3e646c")
 
-btns = {}
-labels = {}
-#btn = Button(frame, text="Click", fg="#263d42")
-#btn.place(relwidth=0.15, relheight=0.15, rely=0.1, relx=0.1)
+        self.frame.place(relwidth=0.85, relheight=0.85, rely=0.1, relx=0.1)
 
-#btn.grid(row=0)
+        self.btns = {}
+        self.labels = {}
+
+        self.test_list = []
+
+        self.btns["run"] = Button(self.frame, text="Play Trading Period", fg="#263d42")
+        self.btns["run"].grid(row=0)
+        self.btns["run"].bind("<Button-1>", self.run_test)
+
+        self.btns["graph"] = Button(self.frame, text="Create Graph", fg="#263d42")
+        self.btns["graph"].grid(row=0, column=1)
+        self.btns["graph"].bind("<Button-1>", self.update_chart)
+
+        self.labels["run"] = Label(self.frame, text="Not Running")
+
+    def run_test(self, event):
+        print("running test")
+        self.states["testing"] = True
+        self.market = Market_Environment("processed stock data")
+        self.test_list = trader.run_test(self.market)
+        self.states["testing"] = False
+        self.states["tested"] = True
+
+    def update_chart(self, event):
+        data = []
+        for item in self.test_list:
+            data.append(item["value"])
+
+        plt.plot(data)
+        plt.show()
+
+    def pie_chart(self, event):
+        #https://matplotlib.org/3.1.1/gallery/pie_and_polar_charts/pie_features.html
+        data = []
+        for item in self.test_list:
+            data.append(item["allocation"])
+
+        labels = 'Frogs', 'Hogs', 'Dogs', 'Logs'
+        sizes = [15, 30, 45, 10]
+        explode = (0, 0.1, 0, 0)  # only "explode" the 2nd slice (i.e. 'Hogs')
+
+        fig1, ax1 = plt.subplots()
+        ax1.pie(sizes, explode=explode, labels=labels, autopct='%1.1f%%',
+                shadow=True, startangle=90)
+        ax1.axis('equal')  # Equal aspect ratio ensures that pie is drawn as a circle.
+
+plt.show()
+
+
+    def run_gui(self):
+        self.root.mainloop()
 
 ##todo list:
 #1 run a trading period with a finished model
 
-btns["run"] = Button(frame, text="Play Trading Period", fg="#263d42")
-btns["run"].grid(row=0)
 
-labels["run"] = Label(frame, text="")
-#labels["run"].grid(row=1, column=0)
+
+
 
 
 #2 create analytics for
@@ -41,5 +91,5 @@ labels["run"] = Label(frame, text="")
 
 
 
-root.mainloop()
+
 
