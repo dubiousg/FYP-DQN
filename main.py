3b96e05f503a9fcc1b894718ace3aa0a1260c3cb
dubiousg <37251424+dubiousg@users.noreply.github.com>
2020-04-18 00:39:47 +0100

    updated

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 #import all project files and libraries
 from model.clean_data import data_cleaner
 from model.market import Market_Environment
+from GUI.app import GUI
 import model.trader as trader
 import pandas as pd
 import numpy as np
@@ -8,17 +9,9 @@ from os.path import isfile, join
 #cleaner = data_cleaner()
 #cleaner.add_files_from_folder("original stock data/")
 
-market_env = Market_Environment("processed stock data")
+#market_env = Market_Environment("processed stock data")
 
 #trader.run(market_env)
-trader.run_test(market_env)
-'''
-index=[0,1,2,3]
-columns=['one', 'two']
-data = np.array([np.arange(2, 10, 2)]*2).T
-
-df = pd.DataFrame(data, index=index, columns=columns)
-#df['one'] = 
-df['one'] = df['one'].div(df['one'][0])
-print(df)
-'''
+#trader.run_test(market_env)
+gui = GUI()
+gui.run_gui()
