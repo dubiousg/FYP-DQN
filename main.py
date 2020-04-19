9f99c2b41042c7c3a17b0c78305fb4746e7b75fd
dubiousg <37251424+dubiousg@users.noreply.github.com>
2020-04-19 21:41:37 +0100

    commented

--- main.py
+++ main.py
@@ -6,12 +6,16 @@ import model.trader as trader
 import pandas as pd
 import numpy as np
 from os.path import isfile, join
+#cleaner can be called as such:
 #cleaner = data_cleaner()
 #cleaner.add_files_from_folder("original stock data/")
 
+#market can be called as such:
 #market_env = Market_Environment("processed stock data")
 
+#trader can be called as such:
 #trader.run(market_env)
 #trader.run_test(market_env)
+
 gui = GUI()
 gui.run_gui()
